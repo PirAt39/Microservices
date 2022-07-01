7d105131a40ae1d72cd8be225b7ce1d78854ee38
Anurag Gupta <iam.anurag@gmail.com>
2022-07-01 14:38:58 +0530

    first commit

---
+++ limit-service/src/main/java/com/microservices/limitservice/controller/LimitsController.java
@@ -0,0 +1,19 @@
+package com.microservices.limitservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.limitservice.bean.Limit;
+import com.microservices.limitservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public Limit retrieveLimits() {
+		return new Limit(configuration.getMinimum(), configuration.getMaximum());
+	}
+}
